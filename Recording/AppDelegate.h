ea96e7cbf6f15c9eee24945f140c89a112c219de
Olivia Miller <oam2111@columbia.edu>
2016-07-06 11:52:08 -0400

    Initial Commit

---
+++ Recording/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Recording
+//
+//  Created by Olivia Miller on 7/6/16.
+//  Copyright © 2016 Olivia Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
